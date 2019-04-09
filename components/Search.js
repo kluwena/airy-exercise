1c9ec5b30d50f919a88d513d5b2c20d70b64f280
kluwena <karelluwena@gmail.com>
2019-04-09 13:32:22 +0700

    added styling for feed

--- components/Search.js
+++ components/Search.js
@@ -51,17 +51,48 @@ class Search extends React.Component {
         <div className="tweet-container">
           {this.state.tweets.map((item, key) => (
             <div key={key} className="tweet-item">
-              <img 
-                className="profile-image"
-                src={item.user.profile_image_url_https}
-              />
-              <p>{item.user.name}</p>
+              <div className="row">
+                <div className="column">
+                  <img 
+                    className="profile-image"
+                    src={item.user.profile_image_url_https}
+                  />
+                </div>
+                <div className="column">
+                  <p>{item.user.name}</p>
+                </div>
+              </div>
               <a href={`https://twitter.com/${item.user.screen_name}/statuses/${item.id_str}`}>
                 {item.text}
               </a>
             </div>
           ))}
         </div>
+      <style jsx>{`
+      
+        .tweet-item{
+          height: 20px
+          margin-right: auto;
+          margin-left:  auto;
+          max-width: 960px;
+          padding-right: 10px;
+          padding-left:  10px;
+          border-style: solid;
+          border-width: 1px;
+        }
+
+        .column {
+          float: left;
+          width: 20%;
+        }
+
+        .row:after {
+          content: "";
+          display: table;
+          clear: both;
+        }
+      
+      `}</style>
       </div>
     )
   }
